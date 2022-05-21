9426be518d14364c01133800531036e4fb034904
Konrad Gnat <konradgnat@Konrads-MacBook-Pro-2.local>
2022-05-20 21:37:33 -0400

    css color palette

--- pages/_app.js
+++ pages/_app.js
@@ -9,11 +9,35 @@ const getLibrary = (provider) => {
   return library;
 };
 
+//https://coolors.co/palette/011627-fdfffc-2ec4b6-e71d36-ff9f1c
+const theme = extendTheme({
+  config: {
+    useSystemColorMode: false,
+    initialColorMode: "light",
+  },
+  colors: {
+    brand: {
+      /*
+      --rich-black-fogra-29: #011627ff;
+      --baby-powder: #fdfffcff;
+      --tiffany-blue: #2ec4b6ff;
+      --rose-madder: #e71d36ff;
+      --orange-peel: #ff9f1cff;
+      */
+      100: "#011627ff",
+      200: "#fdfffcff",
+      300: "#2ec4b6ff",
+      400: "#e71d36ff",
+      500: "#ff9f1cff",
+    },
+  },
+});
+
 const App = ({ Component, pageProps }) => {
   return (
     <TinaProvider>
       <Web3ReactProvider getLibrary={getLibrary}>
-        <ChakraProvider>
+        <ChakraProvider theme={theme}>
           <Component {...pageProps} />
         </ChakraProvider>
       </Web3ReactProvider>
